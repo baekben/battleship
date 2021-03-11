9872fcc408564f68484f30b4037627a997a0e146
Ben <ben10baek@hotmail.com>
2021-03-10 17:25:52 -0800

    Tested gameboard for attacks on ship

--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -7,3 +7,11 @@ test('Add ship', () => {
 	let expected = 3;
 	expect(board.ships[0].type).toBe(expected);
 });
+
+test('Recieve an attack', () => {
+	board.placeShip(3);
+	let curShip = board.ships[0];
+	board.recieveAttack(curShip, 0);
+	const expected = [1, 0, 0];
+	expect(board.ships[0].lives).toEqual(expected);
+});
