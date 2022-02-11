ad074fb934cbcaaa6c30c2352eb59095586eb0cc
Ben <ben10baek@hotmail.com>
2022-02-11 12:20:29 -0800

    Fixed drag feature so that old ship will disappear off board after drag event occurs

--- src/Drag.js
+++ src/Drag.js
@@ -2,33 +2,56 @@ import gameRender from './GameRender';
 const Drag = (player, board) => {
 	let selectedShip;
 	let shipIndex;
+	let group = [];
 
 	const getShipIndex = (e) => {
 		shipIndex = Number(e.target.dataset.index);
+		console.log(shipIndex);
 	};
 
 	const dragBegin = (e) => {
 		selectedShip = e.target;
-		console.log(selectedShip);
+		let item = e.target.dataset.ship;
+		let grabship = document.querySelectorAll('.occupied');
+		for (let ship of grabship) {
+			let child = ship.querySelector('.ship');
+			if (child.classList.contains(`${item}`)) {
+				group.push(child); //grab the ship that is going to be dragged. Then I can remove the previous location
+			}
+		}
+		console.log(group);
 	};
 
 	const dragDrop = (e) => {
 		const cell = e.target;
 		const p1Ship = player.getFleet()[selectedShip.dataset.ship];
+		console.log(p1Ship);
 		const isHorizontal = p1Ship.getDirection() === 'horizontal';
 		const y = Number(cell.dataset.y) - (isHorizontal ? 0 : shipIndex);
 		const x = Number(cell.dataset.x) - (isHorizontal ? shipIndex : 0);
 
+		//outcome after placing ship
 		const outcome = board.placeShip(p1Ship, y, x);
 		if (outcome) {
+			//want to remove old ship from array and rerender board
+			//get old ship: in group
+			//get board and set points in array to 0
+			//have gameRender run
+			group.forEach((part) => {
+				let x1 = part.dataset.x;
+				let y1 = part.dataset.y;
+				board.getBoard()[y1][x1] = 0;
+			});
 			gameRender.onScreenGrid(document.querySelector('.p1Grid'), board, player.getUser());
 			gameRender.setShipIndexes(player.getFleet());
 			addDragDropEventListeners();
+			console.log(selectedShip.parentElement);
 			selectedShip.parentElement.removeChild(selectedShip);
 			if (board.allShipsPlaced()) {
 				document.querySelector('.start').className = 'start';
 			}
 		}
+		console.log(board.getBoard());
 	};
 
 	const dragOver = (e) => e.preventDefault();
